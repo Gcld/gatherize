ddfe5e630e1ec4e39f1f6b67be74ca7851f78877
Gcld <gabrielcostalima21@gmail.com>
2024-12-18 10:40:15 -0300

    feat: Inputs do Sign Up
    style: mudança no estilo do Input de aniversário

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,50 @@
+'use client'
+
+import { LuArrowLeft, LuCalendar, LuLock, LuMail, LuUser, LuUserPen } from "react-icons/lu";
+import { Container, InputBox, InputWrapper, SignInButton, SignInput, SignUpDiv, SignUpDivReturn } from "./styled";
+
+
+export default function Login() {
+    return (
+        <Container>
+            <InputBox>
+                <SignUpDivReturn>
+                    <LuArrowLeft size={24} color="var(--primaryDarkZaori)" />
+                    <h2>Sign Up</h2>
+                </SignUpDivReturn>
+                <h4>Let us help you gatherize with your friends</h4>
+                <InputWrapper>
+                    <LuUserPen size={16} color="var(--primaryDarkZaori)" />
+                    <SignInput type="text" placeholder="Full Name" />
+                </InputWrapper>
+                <InputWrapper>
+                    <LuUser size={16} color="var(--primaryDarkZaori)" />
+                    <SignInput type="text" placeholder="Username" />
+                </InputWrapper>
+                <InputWrapper>
+                    <LuCalendar size={16} color="var(--primaryDarkZaori)" />
+                    <SignInput type="date" placeholder="dd/mm/aaaa" />
+                </InputWrapper>
+                <InputWrapper>
+                    <LuMail size={16} color="var(--primaryDarkZaori)" />
+                    <SignInput type="text" placeholder="Email" />
+                </InputWrapper>
+                <InputWrapper>
+                    <LuLock size={16} color="var(--primaryDarkZaori)" />
+                    <SignInput type="password" placeholder="Password"/>
+                </InputWrapper>
+                <InputWrapper>
+                    <LuLock size={16} color="var(--primaryDarkZaori)" />
+                    <SignInput type="password" placeholder="Confirm Password" />
+                </InputWrapper>
+                <SignInButton>
+                    <h3>Sign In</h3>
+                </SignInButton>
+                <SignUpDiv>
+                    <h4>New to Gatherize?</h4>
+                    <h4 className="signUp">Join Now</h4>
+                </SignUpDiv>
+            </InputBox>
+        </Container>
+    );
+}
---
+++ src/app/signup/styled.ts
@@ -0,0 +1,145 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 568px; 
+    background-image: url('/background.jpg');
+    background-size: cover;
+    background-position: center; 
+    background-repeat: no-repeat; 
+`;
+
+export const InputBox = styled.div`
+    display: flex;
+    width: 280px;
+    height: 440px;
+    flex-direction: column;
+    justify-content: center;
+    align-items: left;
+    gap: 10px;
+    padding: 0 15px;
+    background-color: var(--darkZaori);
+    border-radius: 30px;
+
+    h2{
+        width: 100%;
+        font-size: 24px;
+        font-weight: bold;
+        color: var(--primaryLightZaori);
+    }
+
+    h4{
+        font-size: 12px;
+        font-weight: normal;
+        color: var(--primaryLightZaori);
+        line-height: 130%;
+        letter-spacing: 0%;
+    }
+`;
+
+export const SignUpDivReturn = styled.div`
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+
+    h2{
+        width: 100%;
+        font-size: 24px;
+        font-weight: bold;
+        color: var(--primaryLightZaori);
+    }
+`;
+
+export const InputWrapper = styled.div`
+    position: relative;
+    width: 100%;
+    display: flex;
+    align-items: center;
+
+    svg {
+        position: absolute;
+        left: 10px;
+        top: 50%;
+        transform: translateY(-50%);
+    }
+`;
+
+export const SignInput = styled.input`
+    width: 100%;
+    padding: 5px 10px 5px 30px; 
+    border-radius: 10px;
+    background-color: transparent;
+    border: 1.5px solid var(--primaryLightZaori);
+    color: var(--primaryLightZaori);
+    font-size: 14px;
+
+    &::placeholder {
+        color: var(--grayZaori); 
+        opacity: 0.7;
+    }
+
+    &:focus {
+        outline: none;
+        border-color: var(--primaryDarkZaori);
+    }
+
+    &[type="date"]::-webkit-calendar-picker-indicator {
+        display: none;
+    }
+
+    &[type="date"]::-webkit-datetime-edit-text,
+    &[type="date"]::-webkit-datetime-edit-month-field,
+    &[type="date"]::-webkit-datetime-edit-day-field,
+    &[type="date"]::-webkit-datetime-edit-year-field {
+        color: var(--grayZaori);
+    }
+
+    &[type="password"]::-ms-reveal,
+    &[type="password"]::-ms-clear {
+        display: none;
+    }
+`;
+
+export const SignInButton = styled.div`
+    display: flex;
+    width: 100%;
+    background-color: var(--primaryDarkZaori);
+    border-radius: 10px;
+    h3{
+        width: 100%;
+        font-size: 16px;
+        font-weight: bold;
+        color: black;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        padding: 5px;
+    }
+`;
+
+export const SignUpDiv = styled.div`
+    display: flex;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    padding: 5px;
+    h4{
+        font-size: 14px;
+        font-weight: normal;
+        color: var(--primaryLightZaori);
+        line-height: 130%;
+    }
+
+    .signUp{
+        font-size: 14px;
+        font-weight: normal;
+        color: var(--primaryDarkZaori);
+        line-height: 130%;
+    }
+`;
