c09b8edfd4b126574c73feecceb2b932e26f565e
gabriel.lima <gabrielcostalima21@gmail.com>
2024-12-19 10:06:52 -0300

    feat: página de detalhamento do evento

--- src/app/event/id/page.tsx
+++ src/app/event/id/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
-import { LuArrowLeft, LuShare } from "react-icons/lu";
-import { Container, EventButton, EventButtonsDiv, EventPicture } from "./styled";
+import { LuArrowLeft, LuCircleCheck, LuShare } from "react-icons/lu";
+import { Container, EventButton, EventButtonsDiv, EventDateAndLocationDiv, EventDescriptionAndButtonDiv, EventDescriptionDiv, EventPicture, SubscribeButton, TextBlock, TitleAndDescriptionDiv } from "./styled";
 
 export default function EventDetail(){
     return (
@@ -12,6 +12,34 @@ export default function EventDetail(){
                     <EventButton><LuShare color="var(--primaryDarkZaori)" size={19}/></EventButton>
                 </EventButtonsDiv>
             </EventPicture>
+            <TitleAndDescriptionDiv>
+                <h1>Headline 1</h1>
+                <h4>Description event</h4>
+            </TitleAndDescriptionDiv>
+            <EventDateAndLocationDiv>
+                <TextBlock>
+                    <h5>Date:</h5>
+                    <h6>Monday, Dec 9</h6>
+                    <h6>6:00 PM</h6>
+                </TextBlock>
+                <TextBlock>
+                    <h5>Location:</h5>
+                    <h6>Nova Parnamirim</h6>
+                    <h6>Zona Abissal</h6>
+                </TextBlock>
+            </EventDateAndLocationDiv>
+            <EventDescriptionAndButtonDiv>
+                <EventDescriptionDiv>
+                    <TextBlock>
+                        <h4>About event</h4>
+                        <h6>Event description here, Lorem ipsum dolor sit amet, consectetur adipiscing elit. In molestie euismod interdum. Aenean vel est tellus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc scelerisque ut risus non... </h6>
+                    </TextBlock>
+                </EventDescriptionDiv>
+                <SubscribeButton>
+                    <LuCircleCheck color="black" size={24}/>
+                    <h1>Subscribe</h1>
+                </SubscribeButton>
+            </EventDescriptionAndButtonDiv>
         </Container>
     )
 }
--- src/app/event/id/styled.ts
+++ src/app/event/id/styled.ts
@@ -4,6 +4,8 @@ export const Container = styled.div`
     display: flex;
     flex-direction: column;
     width: 100%;
+    min-height: 100vh;
+    justify-content: space-between;
 `;
 
 export const EventPicture = styled.div`
@@ -28,4 +30,97 @@ export const EventButton = styled.div`
     background-color: var(--darkZaori);
     padding: 5px;
     justify-content: space-between;
+`;
+
+export const TitleAndDescriptionDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    gap: 8px;
+    padding: 0 16px;
+    align-items: top center;
+
+    h1{
+        font-size: 24px;
+        font-weight: bold;
+        color: white;
+        line-height: 120%;
+    }
+
+    h4{
+        font-size: 16px;
+        font-weight: normal;
+        color: var(--grayZaori);
+        line-height: 130%;
+    }
+`;
+
+export const TextBlock = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+
+    h4{
+        font-size: 16px;
+        font-weight: bold;
+        color: white;
+        line-height: 130%;
+    }
+
+    h5{
+        font-size: 14px;
+        font-weight: bold;
+        color: white;
+        line-height: 130%;
+    }
+
+    h6{
+        font-size: 14px;
+        font-weight: normal;
+        color: var(--grayZaori);
+        line-height: 130%;
+    }
+`;
+
+export const EventDateAndLocationDiv = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 0 8px;
+`;
+
+export const EventDescriptionAndButtonDiv = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    background-color: var(--primaryDarkZaori);
+`;
+
+export const EventDescriptionDiv = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: 8px 8px;
+    background-color: black;
+    border-radius: 0 0 10px 10px;
+`;
+
+export const SubscribeButton = styled.div`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 10vh;
+    background-color: var(--primaryDarkZaori);
+    justify-content: center;
+    align-items: center;
+    gap: 4px;
+    padding: 8px;
+
+    h1{
+        font-size: 24px;
+        font-weight: bold;
+        color: black;
+        line-height: 120%;
+    }
 `;
