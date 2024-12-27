f98117cd5e5300caf0868a30f7141495d70b9849
Gcld <gabrielcostalima21@gmail.com>
2024-12-27 11:07:20 -0300

    feat: página de detalhamento de admin criada
    feat: botões de admin adicionados

---
+++ src/app/event/[id]/admin/page.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import React from 'react';
+import { LuArrowLeft, LuCircleCheck, LuClipboardPen, LuShare, LuTrash2, LuX } from "react-icons/lu";
+import { useSubscription } from "@/contexts/SubscriptionContext";
+import Link from 'next/link';
+import { Container, EventButton, EventButtonsDiv, EventDateAndLocationDiv, EventDescriptionAndButtonDiv, EventDescriptionDiv, EventPicture, SubscribeButton, TextBlock, TitleAndDescriptionDiv } from './styled';
+
+
+export default function EventDetailAdmin(){
+    const temporaryEventId = 1;
+    const { toggleSubscription, isSubscribed } = useSubscription();
+    const eventSubscribed = isSubscribed(temporaryEventId);
+
+    const handleSubscribe = (e?: React.MouseEvent) => {
+        // Previne a propagação do evento se for um evento de clique
+        if (e) {
+            e.stopPropagation();
+        }
+        toggleSubscription(temporaryEventId);
+    };
+
+    return (
+        <Container>
+            <EventPicture>
+                <EventButtonsDiv>
+                    <Link href="/" passHref><EventButton><LuArrowLeft className="icon"/></EventButton></Link>
+                    <EventButton>
+                        <LuClipboardPen className="icon"/>
+                        <h3>Edit Event</h3>
+                    </EventButton>
+                    <EventButton>
+                        <LuTrash2 className="icon"/>
+                        <h3>Delete Event</h3>
+                    </EventButton>
+                    <EventButton><LuShare className="icon"/></EventButton>
+                </EventButtonsDiv>
+            </EventPicture>
+            <TitleAndDescriptionDiv>
+                <h1>Headline 1</h1>
+                <h4>Description event</h4>
+            </TitleAndDescriptionDiv>
+            <EventDateAndLocationDiv>
+                <TextBlock>
+                    <h5>Date:</h5>
+                    <h6>Monday, Dec 9</h6>
+                    <h6>6:00 PM</h6>
+                </TextBlock>
+                <TextBlock>
+                    <h5>Location:</h5>
+                    <h6>Nova Parnamirim</h6>
+                    <h6>Zona Abissal</h6>
+                </TextBlock>
+            </EventDateAndLocationDiv>
+            <EventDescriptionAndButtonDiv $isSubscribed={eventSubscribed} onClick={(e) => handleSubscribe(e)}>
+                <EventDescriptionDiv>
+                    <TextBlock>
+                        <h4>About event</h4>
+                        <h6>Event description here, Lorem ipsum dolor sit amet, consectetur adipiscing elit. In molestie euismod interdum. Aenean vel est tellus. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc scelerisque ut risus non... </h6>
+                    </TextBlock>
+                </EventDescriptionDiv>
+                <SubscribeButton 
+                    $isSubscribed={eventSubscribed} 
+                    onClick={(e) => handleSubscribe(e)}
+                >
+                    {eventSubscribed ? (
+                        <LuX className="subscribeIcon"/>
+                    ) : (
+                        <LuCircleCheck className="subscribeIcon"/>
+                    )}
+                    <h1>{eventSubscribed ? 'Cancel Subscription' : 'Subscribe'}</h1>
+                </SubscribeButton>
+            </EventDescriptionAndButtonDiv>
+        </Container>
+    )
+}
---
+++ src/app/event/[id]/admin/styled.tsx
@@ -0,0 +1,205 @@
+import styled from "styled-components";
+
+interface SubscribeButtonProps {
+    $isSubscribed: boolean;
+}
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    min-height: 100vh;
+    justify-content: space-between;
+`;
+
+export const EventPicture = styled.div`
+    width: 100%;
+    height: 35vh;
+    background-color: var(--primaryLightZaori);
+    align-items: top;
+    padding: 10px 5px;
+`;
+
+export const EventButtonsDiv = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`;
+
+export const EventButton = styled.div`
+    display: flex;
+    flex-direction: row;
+    border-radius: 5px;
+    background-color: var(--darkZaori);
+    padding: 3px;
+    justify-content: center;
+    align-items: center;
+    gap: 2px;
+
+    .icon{
+        height: 18px;
+        width: 18px;
+        color: var(--primaryDarkZaori);
+    }
+
+    h3{
+        font-size: 14px;
+        font-weight: bold;
+        line-height: 120%;
+    }
+
+    @media (min-width: 768px){
+        .icon{
+            height: 22px;
+            width: 22px;
+        }
+    }
+`;
+
+export const TitleAndDescriptionDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    gap: 8px;
+    padding: 0 16px;
+    align-items: top center;
+
+    h1{
+        font-size: 24px;
+        font-weight: bold;
+        color: white;
+        line-height: 120%;
+    }
+
+    h4{
+        font-size: 16px;
+        font-weight: normal;
+        color: var(--grayZaori);
+        line-height: 130%;
+    }
+
+    @media (min-width: 768px){
+        h1{
+            font-size: 48px;
+            line-height: 110%;
+        }
+
+        h4{
+            font-size: 18px;
+            line-height: 120%;
+        }
+    }
+`;
+
+export const TextBlock = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 8px;
+
+    h4{
+        font-size: 16px;
+        font-weight: bold;
+        color: white;
+        line-height: 120%;
+    }
+
+    h5{
+        font-size: 14px;
+        font-weight: bold;
+        color: white;
+        line-height: 120%;
+    }
+
+    h6{
+        font-size: 14px;
+        font-weight: normal;
+        color: var(--grayZaori);
+        line-height: 120%;
+    }
+
+    @media (min-width: 768px){
+        h4{
+            font-size: 18px;
+            line-height: 120%;
+        }
+        h5{
+            font-size: 16px;
+            line-height: 120%;
+        }
+        h6{
+        font-size: 16px;
+        font-weight: normal;
+        color: var(--grayZaori);
+        line-height: 120%;
+    }
+    }
+`;
+
+export const EventDateAndLocationDiv = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 0 8px;
+`;
+
+export const EventDescriptionAndButtonDiv = styled.div<SubscribeButtonProps>`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    background-color: ${props => props.$isSubscribed ? 'red' : 'var(--primaryDarkZaori)'};
+`;
+
+export const EventDescriptionDiv = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    padding: 8px 8px;
+    background-color: black;
+    border-radius: 0 0 10px 10px;
+`;
+
+export const SubscribeButton = styled.div<SubscribeButtonProps>`
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    height: 10vh;
+    background-color: ${props => props.$isSubscribed ? 'red' : 'var(--primaryDarkZaori)'};
+    justify-content: center;
+    align-items: center;
+    gap: 4px;
+    padding: 8px;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+
+    &:hover {
+        background-color: ${props => props.$isSubscribed ? 'red' : 'var(--primaryZaori)'};
+    }
+
+    .subscribeIcon{
+        height: 24px;
+        width: 24px;
+        color: ${props => props.$isSubscribed ? 'white' : 'black'};
+    }
+
+    h1{
+        font-size: 24px;
+        font-weight: bold;
+        color: ${props => props.$isSubscribed ? 'white' : 'black'};
+        line-height: 120%;
+    }
+
+    @media (min-width: 768px){
+        height: 10vh;
+        .subscribeIcon{
+            height: 32px;
+            width: 32px;
+        }
+
+        h1{
+            font-size: 32px;
+            line-height: 110%;
+        }
+    }
+`;
