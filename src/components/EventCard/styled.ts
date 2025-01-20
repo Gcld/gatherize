4f8be2360f9dfd307e9a3f23d4db79c548415d7b
gabriel.lima <gabrielcostalima21@gmail.com>
2025-01-20 08:46:37 -0300

    style: mudanças no EventCard

--- src/components/EventCard/styled.ts
+++ src/components/EventCard/styled.ts
@@ -8,9 +8,10 @@ interface EventButtonProps {
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    justify-content: space-between;
     align-items: center;
     width: 100%;
+    height: 400px; 
     border-radius: 10px;
     padding: 2px;
     gap: 8px;
@@ -97,28 +98,35 @@ export const EventContent = styled.div`
     align-items: left;
     padding: 0px 5px;
     gap: 8px;
+    flex-grow: 1; 
 
     h1{
         font-size: 24px;
         font-weight: bold;
         line-height: 120%;
         color: black;
+        max-height: 2.4em; 
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 2;
+        -webkit-box-orient: vertical;
     }
 
     @media (min-width: 768px){
         padding: 0px 8px;
         gap: 15px;
         h1{
-            font-size: 48px;
+            font-size: 28px;
             line-height: 110%;
         }
     }
 
-    @media (min-width: 768px){
+    @media (min-width: 1024px){
         padding: 0px 10px;
         gap: 18px;
         h1{
-            font-size: 48px;
+            font-size: 32px;
             line-height: 110%;
         }
     }
@@ -132,6 +140,9 @@ export const EventLocation = styled.div`
         font-weight: normal;
         line-height: 120%;
         color: var(--darkZaori);
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
     }
 
     @media (min-width: 768px){
@@ -142,7 +153,7 @@ export const EventLocation = styled.div`
 
     @media (min-width: 1024px){
         h4{
-            font-size: 18px;
+            font-size: 16px;
         }
     }
 `;
@@ -237,7 +248,7 @@ export const EventButton = styled.div<EventButtonProps>`
 
     @media (min-width: 1024px) {
         h2 {
-            font-size: 24px;
+            font-size: 20px;
         }
     }
 
